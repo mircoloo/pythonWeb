29e0c16a6471bf902ec0b615a53598a364984b86
mircoloo <bismirco@gmail.com>
2022-03-26 09:26:44 +0100

    update

---
+++ main.py
@@ -0,0 +1,7 @@
+#!python3
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,16 @@
+#!python3
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asdih32dsd'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')        
+    app.register_blueprint(auth, url_prefix='/')
+
+
     return app
 
